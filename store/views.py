e262974818ceb30e4812fb7acb5b813c0f86b734
nischala342 <pnischala10@gmail.com>
2021-05-12 17:49:27 +0530

    Added filtering by categories

--- store/views.py
+++ store/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from  django.http import HttpResponse
 from .models.products import Product
+from .models.category import Category
+
 # Create your views here.
 def index(request):
-    prds = Product.get_all_products()
-    print(prds)
-    return render(request,'index.html',{'products' : prds})
+    products = None
+    categories = Category.get_all_categories()
+    #return render(request, 'orders/order.html')
+    categoryID = request.GET.get('category')
+    if categoryID:
+        products = Product.get_all_products_by_categoryid(categoryID)
+    else:
+        products = Product.get_all_products()
+
+    data = {}
+    data['products'] = products
+    data['categories'] = categories
+    return render(request,'index.html',data)
