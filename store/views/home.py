0d6475792eb8ca72f9436c7433dc869f31a416ba
Meghana1603 <meghana3812@gmail.com>
2021-05-17 17:41:09 +0530

    added return to same product

--- store/views/home.py
+++ store/views/home.py
@@ -1,24 +1,52 @@
 from django.shortcuts import render, redirect
 from store.models.category import Category
 from store.models.products import Product
-
+from django.views import View
 
 # Create your views here.
-def index(request):
-    products = None
-    categories = Category.get_all_categories()
-    # return render(request, 'orders/order.html')
-    categoryID = request.GET.get('category')
-    if categoryID:
-        products = Product.get_all_products_by_categoryid(categoryID)
-    else:
-        products = Product.get_all_products()
+class Index(View):
+    def post(self, request):
+        product = request.POST.get('product')
+        remove = request.Post.get('remove')
+        cart = request.session.get('cart')
+        if cart:
+            quantity = cart.get(product)
+            if quantity:
+                if remove:
+                    if quantity<=1:
+                        cart.pop(product)
+                    else:
+                        cart[product] = quantity-1
+                else:
+                    cart[product] = quantity+1
+            else:
+                cart[product] = 1
+        else:
+            cart = {}
+            cart[product] = 1
+        request.session['cart'] = cart
+        return redirect('Homepage')
 
-    data = {}
-    data['products'] = products
-    data['categories'] = categories
-    # print('You are: ',request.session.get('email'))
-    return render(request, 'index.html', data)
+    def get(self, request):
+        cart = request.session.get('cart')
+        if not cart:
+            request.session.cart = {}
+        products = None
+        categories = Category.get_all_categories()
+        # return render(request, 'orders/order.html')
+        categoryID = request.GET.get('category')
+        if categoryID:
+            products = Product.get_all_products_by_categoryid(categoryID)
+        else:
+            products = Product.get_all_products()
+
+        data = {}
+        data['products'] = products
+        data['categories'] = categories
+        # print('You are: ',request.session.get('email'))
+        return render(request, 'index.html', data)
+
+
 
 
 
