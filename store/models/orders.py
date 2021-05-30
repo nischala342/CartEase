1b469763b7ccdd4a1c861a588486c90e73432fe9
nischala342 <pnischala10@gmail.com>
2021-05-30 16:02:35 +0530

    Middleware Activation and Cart count display

--- store/models/orders.py
+++ store/models/orders.py
@@ -11,10 +11,12 @@ class Order(models.Model):
     address = models.CharField(max_length=250, default='',blank=True)
     phone = models.CharField(max_length=10,default='',blank=True)
     date = models.DateField(default=datetime.datetime.today)
+    status = models.BooleanField(default=False)
+
 
     def placeOrder(self):
         self.save()
 
     @staticmethod
     def get_orders_by_customer(customer_id):
-        return Order.objects.filter(customer = customer_id)
+        return Order.objects.filter(customer = customer_id).order_by('-date')
--- store/views/orders.py
+++ store/views/orders.py
@@ -4,10 +4,13 @@ from store.models.customer import Customer
 from django.views import View
 from store.models.products import Product
 from store.models.orders import Order
-
+from store.middlewares.auth import auth_middleware
+from django.utils.decorators import method_decorator
 
 class OrderView(View):
+
     def get(self, request):
         customer = request.session.get('customer')
         order = Order.get_orders_by_customer(customer)
+        #order.reverse()
         return render(request,'orders.html', {'orders': order})
