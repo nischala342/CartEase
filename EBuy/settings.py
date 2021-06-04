e3aafedc2f39064c3b0ccbe9cefda54451093303
nischala342 <pnischala10@gmail.com>
2021-06-04 17:50:46 +0530

    Static files addition

--- EBuy/settings.py
+++ EBuy/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATIC_ROOT = 'static'
 STATIC_URL = '/static/'
 MEDIA_URL = "/images/download/"
 MEDIA_ROOT = BASE_DIR
